746bce81dd88d7a2c3c4e997d9d858ccccbcae91
PrathamMittal-Tech <Prathamv0106@gmail.com>
2024-09-30 19:49:43 +0530

    Additions

--- JS/script.js
+++
@@ -1,7 +0,0 @@
-let input = document.querySelector("#contact-input");
-
-input.addEventListener("keypress", function(event) {
-    if (event.key === "Enter") {
-        input.innerText = "";
-    }
-  });
